10bb2b3f35046dfee673b7aee176e4febf68c703
ControlCenterSoft <40738562+ControlCenterSoft@users.noreply.github.com>
2026-09-12 02:25:35 +0300

    HC 0.57: activate crash-safe enrollment execution recovery

--- product/control-plane/src/home_center/runtime.py
+++ product/control-plane/src/home_center/runtime.py
@@ -15,7 +15,7 @@ from .automation_execution import AutomationPlanningService
 from .auth import LoginRateLimiter, SessionManager
 from .certificate_api import CertificateLifecycleApi, runtime_certificate_records
 from .config import Config
-from .device_management_enrollment_execution_runtime_safe import SafeDeviceManagementEnrollmentExecutionRuntimeService
+from .device_management_enrollment_execution_recovery import RecoverableDeviceManagementEnrollmentExecutionRuntimeService
 from .device_management_provider_runtime import DeviceManagementProviderRuntimeService
 from .device_management_provider_selection_runtime import DeviceManagementProviderSelectionRuntimeService
 from .external_access import ExternalAccessPolicy, ExternalRequestRateLimiter
@@ -74,7 +74,7 @@ class Runtime:
         self.household_device_enrollment = HouseholdDeviceEnrollmentRuntimeService(self.store)
         self.device_management_providers = DeviceManagementProviderRuntimeService(self.store)
         self.device_management_provider_selection = DeviceManagementProviderSelectionRuntimeService(self.store)
-        self.device_management_enrollment_execution = SafeDeviceManagementEnrollmentExecutionRuntimeService(self.store)
+        self.device_management_enrollment_execution = RecoverableDeviceManagementEnrollmentExecutionRuntimeService(self.store)
         self.reconciler = Reconciler(config, self.store)
 
     def actor_requires_password_change(self, actor: str) -> bool:
