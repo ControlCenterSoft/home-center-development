7fcfee2a5608015b50cc7fe6d78ecc819ccc14da
ControlCenterSoft <40738562+ControlCenterSoft@users.noreply.github.com>
2026-09-12 08:28:32 +0300

    HC 0.58: wire post-cleanup verification runtime

--- product/control-plane/src/home_center/runtime.py
+++ product/control-plane/src/home_center/runtime.py
@@ -15,6 +15,7 @@ from .automation_execution import AutomationPlanningService
 from .auth import LoginRateLimiter, SessionManager
 from .certificate_api import CertificateLifecycleApi, runtime_certificate_records
 from .config import Config
+from .device_management_enrollment_cleanup_verification_runtime import DeviceManagementEnrollmentCleanupVerificationRuntimeService
 from .device_management_enrollment_deenrollment_runtime import DeviceManagementEnrollmentDeenrollmentRuntimeService
 from .device_management_enrollment_execution_recovery import RecoverableDeviceManagementEnrollmentExecutionRuntimeService
 from .device_management_enrollment_managed_state_runtime import DeviceManagementEnrollmentManagedStateRuntimeService
@@ -81,6 +82,7 @@ class Runtime:
         self.device_management_enrollment_verification = DeviceManagementEnrollmentVerificationRuntimeService(self.store)
         self.device_management_enrollment_managed_state = DeviceManagementEnrollmentManagedStateRuntimeService(self.store)
         self.device_management_enrollment_deenrollment = DeviceManagementEnrollmentDeenrollmentRuntimeService(self.store)
+        self.device_management_enrollment_cleanup_verification = DeviceManagementEnrollmentCleanupVerificationRuntimeService(self.store)
         self.reconciler = Reconciler(config, self.store)
 
     def actor_requires_password_change(self, actor: str) -> bool:
