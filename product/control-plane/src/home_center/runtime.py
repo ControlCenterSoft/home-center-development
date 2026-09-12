fd6214a8316e25c932ec91889f448f8611bf051b
ControlCenterSoft <40738562+ControlCenterSoft@users.noreply.github.com>
2026-09-12 05:37:51 +0300

    HC 0.59: compose policy workflow for production boundary

--- product/control-plane/src/home_center/runtime.py
+++ product/control-plane/src/home_center/runtime.py
@@ -23,6 +23,7 @@ from .household_device_management_runtime import HouseholdDeviceManagementRuntim
 from .household_device_runtime import HouseholdDeviceRuntimeService
 from .household_policy_desired_state import HouseholdPolicyDesiredStateService
 from .household_policy_runtime import HouseholdPolicyRuntimeService
+from .household_policy_workflow import HouseholdPolicyWorkflowService
 from .household_runtime import HouseholdRuntimeService
 from .local_admin_auth import LocalAdminCredentialStore
 from .local_admin_change import LocalAdminPasswordChangeClient
@@ -72,6 +73,11 @@ class Runtime:
         self.household = HouseholdRuntimeService(self.store)
         self.household_policy = HouseholdPolicyRuntimeService(self.store)
         self.household_policy_desired_state = HouseholdPolicyDesiredStateService(self.store)
+        self.household_policy_workflow = HouseholdPolicyWorkflowService(
+            self.store,
+            policy_runtime=self.household_policy,
+            desired_state=self.household_policy_desired_state,
+        )
         self.household_devices = HouseholdDeviceRuntimeService(self.store)
         self.household_device_management = HouseholdDeviceManagementRuntimeService(self.store)
         self.household_device_enrollment = HouseholdDeviceEnrollmentRuntimeService(self.store)
