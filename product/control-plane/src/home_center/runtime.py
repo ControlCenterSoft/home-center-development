45db5061e01075bc13cc4e7c8899c3d902bd988e
ControlCenterSoft <40738562+ControlCenterSoft@users.noreply.github.com>
2026-09-12 22:35:00 +0300

    HC 0.59: compose policy enforcement service fail-closed

--- product/control-plane/src/home_center/runtime.py
+++ product/control-plane/src/home_center/runtime.py
@@ -26,6 +26,7 @@ from .external_access import ExternalAccessPolicy, ExternalRequestRateLimiter
 from .household_device_enrollment_runtime import HouseholdDeviceEnrollmentRuntimeService
 from .household_device_management_runtime import HouseholdDeviceManagementRuntimeService
 from .household_device_runtime import HouseholdDeviceRuntimeService
+from .household_policy_enforcement_runtime import HouseholdPolicyEnforcementRuntimeService
 from .household_policy_reconciliation_api_runtime import HouseholdPolicyReconciliationApiService
 from .household_policy_reconciliation_runtime import HouseholdPolicyReconciliationRuntimeService
 from .household_runtime import HouseholdRuntimeService
@@ -80,6 +81,7 @@ class Runtime:
         self.household_devices = HouseholdDeviceRuntimeService(self.store)
         self.household_device_management = HouseholdDeviceManagementRuntimeService(self.store)
         self.household_device_enrollment = HouseholdDeviceEnrollmentRuntimeService(self.store)
+        self.household_policy_enforcement = HouseholdPolicyEnforcementRuntimeService(self.store)
         self.household_policy_reconciliation = HouseholdPolicyReconciliationRuntimeService(self.store)
         self.household_policy_reconciliation_api = HouseholdPolicyReconciliationApiService(
             self.store,
