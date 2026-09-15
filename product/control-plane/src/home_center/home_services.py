3cc18dca8b62cac54898298843237214704d5016
ControlCenterSoft <40738562+ControlCenterSoft@users.noreply.github.com>
2026-09-16 00:32:53 +0300

    HC Modules: fail closed on mismatched service identity (#312)
    * HC Modules: bind service identity to kind
    
    * HC Modules: regress mismatched service identity

--- product/control-plane/src/home_center/home_services.py
+++ product/control-plane/src/home_center/home_services.py
@@ -97,6 +97,8 @@ class HomeServiceProfile:
         object.__setattr__(self, "service_id", _identifier(self.service_id, "invalid_service_id"))
         if not isinstance(self.kind, HomeServiceKind):
             raise HomeServiceCatalogError("invalid_service_kind")
+        if self.service_id != self.kind.value:
+            raise HomeServiceCatalogError("service_identity_mismatch")
         if not isinstance(self.name, str) or not 1 <= len(self.name.strip()) <= 80:
             raise HomeServiceCatalogError("invalid_service_name")
         object.__setattr__(self, "name", self.name.strip())
